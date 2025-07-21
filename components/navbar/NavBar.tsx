26f6a8c4aa595bf08afc1ef833790dc5d90bdff4
GertsDev <hello@gerts.dev>
2025-07-20 20:46:23 -0400

    add navbar

---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+
+const NavBar = () => {
+  return (
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full px-1 py-2 border-b-2  md:px-8 backdrop-blur-md ">
+      <Link href="/">
+        <div className="flex items-center gap-3 ps-4">
+          {/* <Image
+            className="dark:invert-0 invert"
+            src="/logo.png"
+            alt="Graft Logo"
+            width={40}
+            height={40}
+          /> */}
+          <span className="text-2xl font-bold tracking-wide  ">GRAFT</span>
+        </div>
+      </Link>
+      <div className="flex items-center h-16 gap-4 p-4">
+        <ThemeToggle />
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
---
+++ components/navbar/ThemeToggle.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { motion } from "motion/react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+
+const ThemeToggle = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <div className="size-9" aria-hidden="true" />; // Placeholder to maintain layout
+  }
+
+  const isDark = resolvedTheme === "dark";
+
+  return (
+    <Button
+      //key={isDark ? 'sun' : 'moon'}
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(isDark ? "light" : "dark")}
+      aria-label={`Switch to ${isDark ? "light" : "dark"} theme`}
+      className="cursor-pointer group"
+    >
+      <motion.span
+        animate={{
+          rotate: isDark ? 180 : 0,
+        }}
+        transition={{ type: "spring", stiffness: 200, damping: 50 }}
+        className="flex items-center justify-center "
+      >
+        {isDark ? <Sun className="size-6 " /> : <Moon className="size-6 " />}
+      </motion.span>
+    </Button>
+  );
+};
+
+export default ThemeToggle;
